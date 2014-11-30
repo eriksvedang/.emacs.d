afef9c622ea306ee00d5f8554b639cd15ae0bff7
Erik Svedäng <erik.svedang@gmail.com>
2014-11-30 16:47:02 +0100

    ace jump mode

--- init.el
+++ init.el
@@ -2,10 +2,10 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
-	 ;; If there is more than one, they won't work right.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("f464b02a2186c72fcfd1571eb055e3692436d4674de0aec803e4d63978279106" "0a59878fb9407845f4cbc8548aa693e61897989f8a375c501a215efe5d762f22" "ae5b2ccf83a69e804052f6315f300ff6e0d968194abcfe5fcdd326a8807f14e2" "0c5b28806af5b07fc93a64b9a089a57646ff6f2b58f5b53852c12c394433d1de" default))))
+    ("d507a1a88da980c8e2ca87f1ce122b186e8ebba47e03f2eed19cad8cfc472691" "f464b02a2186c72fcfd1571eb055e3692436d4674de0aec803e4d63978279106" "0a59878fb9407845f4cbc8548aa693e61897989f8a375c501a215efe5d762f22" "ae5b2ccf83a69e804052f6315f300ff6e0d968194abcfe5fcdd326a8807f14e2" "0c5b28806af5b07fc93a64b9a089a57646ff6f2b58f5b53852c12c394433d1de" default))))
 
 ;; Damnit, path!
 (let ((paths (mapcar (lambda (i) (concat (getenv "HOME") "/" i))
@@ -62,7 +62,8 @@
 			  'idle-highlight-mode
 			  'find-file-in-project
 			  'restclient
-			  'ido-ubiquitous)
+			  'ido-ubiquitous
+			  'ace-jump-mode)
 
 ;; Startup
 (setq inhibit-splash-screen t)
@@ -98,6 +99,9 @@
 
 ;; Highlight instances of the same symbol
 (idle-highlight-mode 1)
+
+;; Ace jump!
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 ;; Mouse wheel
 (setq mouse-wheel-scroll-amount '(3 ((shift) . 1)))
