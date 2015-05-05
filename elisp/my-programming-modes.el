9c185ae591d273b55b9d5f83ea8b8ebad8b1ed47
Erik <erik.svedang@gmail.com>
2015-05-05 14:50:52 +0200

    uncommented loading of agda

--- elisp/my-programming-modes.el
+++ elisp/my-programming-modes.el
@@ -46,8 +46,8 @@
 
 
 ;; Agda
-(load-file (let ((coding-system-for-read 'utf-8))
-	     (shell-command-to-string "agda-mode locate")))
+;; (load-file (let ((coding-system-for-read 'utf-8))
+;; 	     (shell-command-to-string "agda-mode locate")))
 
 
 
