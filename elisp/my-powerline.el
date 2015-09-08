aea91d344bdbe716fcf51f26eee9df96f0a823f0
Erik <erik.svedang@gmail.com>
2015-09-08 02:03:55 +0200

    new powerline look

--- elisp/my-powerline.el
+++ elisp/my-powerline.el
@@ -55,22 +55,103 @@
 			  )
                      (concat (powerline-render lhs)
                              (powerline-fill mode-line (powerline-width rhs))
-                             (powerline-render rhs)))))))
+                             (powerline-render rhs))))))
+
+  ;; old röd: #ff3f60
+
+  (set-face-attribute 'mode-line nil
+		      :foreground "#fff" :background "#ff3377"
+		      :inverse-video nil
+		      :family "Hasklig"
+		      :height 170
+		      :box '(:line-width 3 :color "#ff3377" :style nil))
+
+  (set-face-attribute 'mode-line-inactive nil
+		      :foreground "gray90" :background "gray60"
+		      :inverse-video nil
+		      :box '(:line-width 3 :color "gray60" :style nil)))
 
 (powerline-erik-theme)
 
-;; old röd: #ff3f60
+(defun powerline-uber-theme ()
+  "uber"
+  (interactive)
+  (setq-default mode-line-format
+		'("%e"
+		  (:eval
+		   (let* ((active (powerline-selected-window-active))
+			  (mode-line (if active 'mode-line 'mode-line-inactive))
+			  (face1 (if active 'powerline-active1 'powerline-inactive1))
+			  (face2 (if active 'powerline-active2 'powerline-inactive2))
+			  (separator-left (intern (format "powerline-%s-%s"
+							  (powerline-current-separator)
+							  (car powerline-default-separator-dir))))
+			  (separator-right (intern (format "powerline-%s-%s"
+							   (powerline-current-separator)
+							   (cdr powerline-default-separator-dir))))
+			  (lhs (list (powerline-raw "%*" nil 'l)
+				     (powerline-buffer-size nil 'l)
+				     (powerline-buffer-id nil 'l)
+				     (powerline-raw " ")
+				     (funcall separator-left mode-line face1)
+				     (powerline-narrow face1 'l)
+				     (powerline-vc face1)))
+			  (rhs (list (powerline-raw global-mode-string face1 'r)
+				     (powerline-raw "%4l" face1 'r)
+				     (powerline-raw ":" face1)
+				     (powerline-raw "%3c" face1 'r)
+				     (funcall separator-right face1 mode-line)
+				     (powerline-raw " ")
+				     (powerline-raw "%6p" nil 'r)
+				     (powerline-hud face2 face1)))
+			  (center (list (powerline-raw " " face1)
+					(funcall separator-left face1 face2)
+					(when (boundp 'erc-modified-channels-object)
+					  (powerline-raw erc-modified-channels-object face2 'l))
+					(powerline-major-mode face2 'l)
+					(powerline-process face2)
+					(powerline-raw " :" face2)
+					(powerline-minor-modes face2 'l)
+					(powerline-raw " " face2)
+					(funcall separator-right face2 face1))))
+		     (concat (powerline-render lhs)
+			     (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+			     (powerline-render center)
+			     (powerline-fill face1 (powerline-width rhs))
+			     (powerline-render rhs))))))
 
-(set-face-attribute 'mode-line nil
-    :foreground "#fff" :background "#ff3377"
-    :inverse-video nil
-    :family "Hasklig"
-    :height 170
-    :box '(:line-width 3 :color "#ff3377" :style nil))
+  (set-face-attribute 'mode-line nil
+              :foreground "#0C3"
+              :background "#000000"
+              :box '(:line-width 1 :color "#000" :style nil)
+              :weight 'bold)
+  
+  (set-face-attribute 'mode-line-buffer-id nil
+              :foreground "white"
+              :weight 'bold)
+  
+  (set-face-attribute 'powerline-active1 nil
+              :foreground "gray20"
+              :background "gray90")
 
-(set-face-attribute 'mode-line-inactive nil
-    :foreground "gray90" :background "gray60"
-    :inverse-video nil
-    :box '(:line-width 3 :color "gray60" :style nil))
+  (set-face-attribute 'powerline-active2 nil
+	      :foreground "gray90"
+	      :background "#0CF") ;; 0CF / FF3377
+  
+  (set-face-attribute 'mode-line-inactive nil
+              :foreground "gray90"
+              :background "gray65"
+              :box nil
+              :weight 'bold)
+  (set-face-attribute 'powerline-inactive1 nil
+              :foreground "#aaa"
+              :background "gray80")
+  (set-face-attribute 'powerline-inactive2 nil
+              :foreground "#ccc"
+              :background "#eee")
+  
+  )
+
+(powerline-uber-theme)
 
 (provide 'my-powerline)
