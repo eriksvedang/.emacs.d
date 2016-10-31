2ae49823841b90c4444d452472674d7eba927dcf
Erik Svedäng <erik.svedang@gmail.com>
2016-10-31 06:29:01 +0100

    comment

--- init.el
+++ init.el
@@ -1,8 +1,4 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Added by Package.el.  This must come before configurations of installed packages.
 (package-initialize)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/my-themes")
