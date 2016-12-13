44ae5bf5381d274ff7d11ba0e590142cb12f3096
Erik Svedäng <erik.svedang@gmail.com>
2016-12-13 21:21:14 +0100

    align!

--- elisp/my-packages.el
+++ elisp/my-packages.el
@@ -9,32 +9,32 @@
 (setq the-packages
       '(
         multiple-cursors
-        smartparens ;; slurp and barf
-        rainbow-delimiters ;; colorful parens
-        rainbow-mode ;; highlight colors in code
-        undo-tree ;; show a graph of edits that can be undone
-        smex ;; better help in minibuffer
-        ido-ubiquitous ;; ido mode for everything
-        company ;; auto completion
-        smooth-scrolling ;; not quite true
-        find-file-in-project ;; looks in current git repo and show all source files
-        avy ;; jump to position in buffer
-        iedit ;; edit all in scope
+        smartparens          ;; Slurp and barf
+        rainbow-delimiters   ;; Colorful parens
+        rainbow-mode         ;; Highlight colors in code
+        undo-tree            ;; Show a graph of edits that can be undone
+        smex                 ;; Better help in minibuffer
+        ido-ubiquitous       ;; Ido mode for everything
+        company              ;; Auto completion
+        smooth-scrolling     ;; Not quite true
+        find-file-in-project ;; Looks in current git repo and show all source files
+        avy                  ;; Jump to position in buffer
+        iedit                ;; Edit all in scope
         exec-path-from-shell
         reveal-in-osx-finder
         google-this
-        expand-region ;; select larger and larger region
-        free-keys ;; show what key combinations that are not taken
-        ace-window ;; select window with number keys
+        expand-region        ;; Select larger and larger region
+        free-keys            ;; Show what key combinations that are not taken
+        ace-window           ;; Select window with number keys
         
         powerline
-        tabbar ;; sometimes tabs are useful
+        tabbar               ;; Sometimes tabs are useful
         
         magit
-        gist ;; send files to Github gist
-        restclient ;; Send web requests from a buffer
+        gist                 ;; Send files to Github gist
+        restclient           ;; Send web requests from a buffer
 
-        org-bullets ;; show nice icons in Org buffers
+        org-bullets          ;; Show nice icons in Org buffers
         
         flycheck
         flymake-easy
@@ -43,30 +43,30 @@
         flymake-hlint
 
         nix-mode
-        nix-buffer ;; "nix-shell for emacs"
+        nix-buffer           ;; "nix-shell for emacs"
 
-        intero ;; Haskell mode that works well with Stack
-        tuareg ;; OCaml
+        intero               ;; Haskell mode that works well with Stack
+        tuareg               ;; OCaml
         idris-mode
         elm-mode
-        sml-mode ;; Standard ML
+        sml-mode             ;; Standard ML
         fsharp-mode
-        cider ;; Clojure
+        cider                ;; Clojure
         racket-mode
         csharp-mode
         rust-mode
-        racer ;; Rust auto complete
+        racer                ;; Rust auto complete
         php-mode
         lua-mode
         swift-mode
         markdown-mode
-        zencoding-mode ;; HTML & CSS
-        js2-mode ;; Javascript
-        web-mode ;; Smart editing of HTML templates
-        cc-mode ;; C
+        zencoding-mode       ;; HTML & CSS
+        js2-mode             ;; Javascript
+        web-mode             ;; Smart editing of HTML templates
+        cc-mode              ;; C
         glsl-mode
-        irony ;; C++ autocomplete
-        gud) ;; GDB debugging
+        irony                ;; C++ autocomplete
+        gud)                 ;; GDB debugging
       )
 
 (mapcar (lambda (package)
