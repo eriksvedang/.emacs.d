d2aa79501f43a9e5edc952e4594b190bbbe2af3b
Erik Svedäng <erik.svedang@gmail.com>
2018-03-02 07:59:19 +0100

    Moved powerline colors over to theme.

--- elisp/my-powerline.el
+++ elisp/my-powerline.el
@@ -7,26 +7,26 @@
   "uber"
   (interactive)
   (setq-default mode-line-format
-		'(" %e"
-		  (:eval
-		   (let* ((active (powerline-selected-window-active))
-			  (mode-line (if active 'mode-line 'mode-line-inactive))
-			  (face1 (if active 'powerline-active1 'powerline-inactive1))
-			  (face2 (if active 'powerline-active2 'powerline-inactive2))
-			  (separator-left (intern (format "powerline-%s-%s"
-							  (powerline-current-separator)
-							  (car powerline-default-separator-dir))))
-			  (separator-right (intern (format "powerline-%s-%s"
-							   (powerline-current-separator)
-							   (cdr powerline-default-separator-dir))))
-			  (lhs (list (powerline-raw " %* " nil 'l)
-				     (powerline-buffer-id nil 'l)
-				     (powerline-raw "  ")
-				     (funcall separator-left mode-line face1)
-				     (powerline-narrow face1 'l)
+        '(" %e"
+          (:eval
+           (let* ((active (powerline-selected-window-active))
+              (mode-line (if active 'mode-line 'mode-line-inactive))
+              (face1 (if active 'powerline-active1 'powerline-inactive1))
+              (face2 (if active 'powerline-active2 'powerline-inactive2))
+              (separator-left (intern (format "powerline-%s-%s"
+                              (powerline-current-separator)
+                              (car powerline-default-separator-dir))))
+              (separator-right (intern (format "powerline-%s-%s"
+                               (powerline-current-separator)
+                               (cdr powerline-default-separator-dir))))
+              (lhs (list (powerline-raw " %* " nil 'l)
+                     (powerline-buffer-id nil 'l)
+                     (powerline-raw "  ")
+                     (funcall separator-left mode-line face1)
+                     (powerline-narrow face1 'l)
                      (powerline-raw " " face1)
-				     (powerline-vc face1)
-				     (powerline-raw " " face1)
+                     (powerline-vc face1)
+                     (powerline-raw " " face1)
                                      (funcall separator-left face1 face2)
                                      (when (boundp 'erc-modified-channels-object)
                                        (powerline-raw erc-modified-channels-object face2 'l))
@@ -35,59 +35,46 @@
                                      (powerline-process face2)
                                      (powerline-raw "  " face2)
                                      (funcall separator-left face2 face1)))
-			  
-			  (rhs (list (powerline-raw global-mode-string face1 'r)
-				     (powerline-raw "%4l" face1 'r)
-				     (powerline-raw ":" face1)
-				     (powerline-raw "%3c " face1 'r)
-				     (funcall separator-right face1 mode-line)
-				     (powerline-raw "    ")
-				     (powerline-raw "%8p" nil 'r)))
-			  (center (list )))
-		     (concat (powerline-render lhs)
-			     (powerline-fill-center face1 (/ (powerline-width center) 2.0))
-			     (powerline-render center)
-			     (powerline-fill face1 (powerline-width rhs))
-			     (powerline-render rhs))))))
+
+              (rhs (list (powerline-raw global-mode-string face1 'r)
+                     (powerline-raw "%4l" face1 'r)
+                     (powerline-raw ":" face1)
+                     (powerline-raw "%3c " face1 'r)
+                     (funcall separator-right face1 mode-line)
+                     (powerline-raw "    ")
+                     (powerline-raw "%8p" nil 'r)))
+              (center (list )))
+             (concat (powerline-render lhs)
+                 (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                 (powerline-render center)
+                 (powerline-fill face1 (powerline-width rhs))
+                 (powerline-render rhs))))))
 
   (if (eq system-type 'darwin)
       (setq powerline-text-height-hack 170)
     (setq powerline-text-height-hack 130))
-    
+
   (set-face-attribute 'mode-line nil
-                      :foreground "#FFF" ;; #0BF
-                      :background "#271c4b" ;; #234
                       :font "Fira Sans"
                       :box nil
                       :weight 'normal
                       :height powerline-text-height-hack)
-  
+
   (set-face-attribute 'mode-line-buffer-id nil
-                      :foreground "white"
                       :weight 'normal)
-  
-  (set-face-attribute 'powerline-active1 nil
-                      :foreground "#271c4b" ;; gray50
-                      :background "gray92")  ;; #ccc
+
+  (set-face-attribute 'powerline-active1 nil)
 
   (set-face-attribute 'powerline-active2 nil
-                      :foreground "white"
-                      :background "#271c4b" ;; #00BF8F #567
-                      :weight 'normal) 
-  
+                      :weight 'normal)
+
   (set-face-attribute 'mode-line-inactive nil
-                      :foreground "gray90"
-                      :background "gray70"
                       :box nil
                       :weight 'bold)
-  
-  (set-face-attribute 'powerline-inactive1 nil
-                      :foreground "#aaa"
-                      :background "gray92")
 
-  (set-face-attribute 'powerline-inactive2 nil
-                      :foreground "gray95"
-                      :background "gray85") 
+  (set-face-attribute 'powerline-inactive1 nil)
+
+  (set-face-attribute 'powerline-inactive2 nil)
   )
 
 (powerline-uber-theme)
--- my-themes/Gammeldansk-theme.el
+++ my-themes/Gammeldansk-theme.el
@@ -3,25 +3,23 @@
 
 (custom-theme-set-faces
  'Gammeldansk
- 
- '(default ((t (:inherit nil
-			 :stipple nil
-			 :background "#FFF"
-			 :foreground "#000"
-			 :inverse-video nil
-			 :box nil
-			 :strike-through nil
-			 :overline nil
-			 :underline nil
-			 :slant normal
-			 :weight normal
-			 :width normal
-			 :font "Hasklig"
-			 :height 170
-			 :foundry "apple"))))
 
-;; #0E9 #A17 #0CF #271c4b 
- 
+ '(default ((t (:inherit nil
+             :stipple nil
+             :background "#FFF"
+             :foreground "#000"
+             :inverse-video nil
+             :box nil
+             :strike-through nil
+             :overline nil
+             :underline nil
+             :slant normal
+             :weight normal
+             :width normal
+             :font "Hasklig"
+             :height 170
+             :foundry "apple"))))
+
  '(font-lock-builtin-face ((t (:weight normal :foreground "#000"))))
  '(font-lock-string-face ((t (:foreground "#A17"))))
  '(font-lock-type-face ((t (:foreground "#000" :weight normal))))
@@ -46,11 +44,11 @@
  '(markdown-header-delimiter-face ((t (:inherit default))))
  '(markdown-markup-face ((t (:inherit default))))
  '(markdown-list-face ((t (:inherit default))))
- 
+
  '(neo-root-dir-face  ((t :height 150 :font "Fira Sans" :foreground "#A17")))
  '(neo-dir-link-face  ((t :height 150 :font "Fira Sans" :foreground "black" :weight bold)))
  '(neo-file-link-face ((t :height 150 :font "Fira Sans" :foreground "black" :weight normal)))
- 
+
  '(font-lock-comment-face ((t (:foreground "gray70" :background "#FFF" :weight normal))))
  '(font-lock-doc-face ((t (:foreground "#271c4b"
                                        :background "white"
@@ -58,29 +56,29 @@
                                        :slant italic
                                        :box))))
  ;;'(font-lock-comment-face ((t (:foreground "#ff0088" :background "#ffecef" :weight bold :slant italic))))
- 
+
  '(cursor ((((background light)) (:background "black")) (((background dark)) (:background "white"))))
  '(fixed-pitch ((t (:family "Monospace"))))
  '(variable-pitch ((t (:family "Sans Serif"))))
  '(escape-glyph ((t (:foreground "#000"))))
- 
+
  '(font-lock-constant-face ((t (:foreground "#000" :weight normal))))
  '(highlight-numbers-number ((t (:foreground "#000" :weight normal))))
- 
+
  '(minibuffer-prompt ((t (:foreground "#000" :weight normal))))
- 
+
  '(highlight ((t (:background "#eeeeee"))))
- 
+
  '(region ((t (:foreground "#000" :background "#0DF"))))
  ;;'(idle-highlight ((t (:background "#f4ffff" :weight bold)))) ;; :underline t
 
  ;;'(font-lock-preprocessor-face ((t :foreground "#fff")))
- 
+
  '(shadow ((((class color grayscale)
-	     (min-colors 88)
-	     (background light)) (:foreground "gray80" :font "Fira Sans" :height 150)) ;; <- Line numbers
-	   )
-	  )
+         (min-colors 88)
+         (background light)) (:foreground "gray80" :font "Fira Sans" :height 150)) ;; <- Line numbers
+       )
+      )
 
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#000"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#000"))))
@@ -96,16 +94,22 @@
 
  '(show-paren-match ((t (:weight bold :underline nil))))
 
- ;;'(mode-line ((t (:foreground "#fff"))))
+ '(mode-line ((t (:foreground "#fff" :background "#271c4b"))))
+ '(mode-line-buffer-id ((t (:foreground "#fff"))))
+ '(powerline-active1 ((t (:foreground "#271c4b" :background "gray92"))))
+ '(powerline-active2 ((t (:foreground "#fff" :background "#271c4b"))))
+ '(mode-line-inactive ((t (:foreground "gray90" :background "gray70"))))
+ '(powerline-inactive1 ((t (:foreground "#aaa" :background "gray92"))))
+ '(powerline-inactive2 ((t (:foreground "gray95" :background "gray85"))))
 
  '(magit-section-heading ((t (:foreground "#0099EE" :weight bold))))
  '(magit-log-author ((t (:foreground "#0099EE"))))
- 
+
  '(secondary-selection ((t (:background "#e5e5e5"))))
  '(trailing-whitespace ((t (:background "#f00"))))
- 
+
  '(font-lock-comment-delimiter-face ((t (:inherit (font-lock-comment-face)))))
-  
+
  '(font-lock-negation-char-face ((t nil)))
  '(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
  '(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
@@ -126,25 +130,25 @@
  '(ido-incomplete-regexp ((t (:foreground "#000")))) ;; Ido face for indicating incomplete regexps. (don't use this either)
 
  '(dired-directory ((t (:foreground "#000" :weight bold))))
- '(dired-ignored ((t (:foreground "#888" :weight bold)))) 
+ '(dired-ignored ((t (:foreground "#888" :weight bold))))
  '(font-lock-warning-face ((t (:inherit (error)))))
  '(cider-repl-err-output-face ((t (:foreground "#FF3F60"))))
- 
+
  '(button ((t (:inherit (link)))))
  '(link ((t (:foreground "#0072b2" :underline (:color foreground-color :style line)))))
  '(link-visited ((t (:inherit (link) :foreground "#cc79a7" :underline (:color foreground-color :style line)))))
  '(fringe ((t (:background "#fff"))))
  '(header-line ((t (:inherit (mode-line) :background "grey90" :foreground "grey20" :box nil))))
  '(tooltip ((t (:inherit (variable-pitch) :background "lightyellow" :foreground "black"))))
- 
- '(isearch ((t (:background "#ff00cc" :foreground "white")))) 
+
+ '(isearch ((t (:background "#ff00cc" :foreground "white"))))
  '(lazy-highlight ((t (:background "#408" :foreground "white"))))
  '(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "#f88"))
-		 (((class color) (min-colors 88) (background dark)) (:background "#f00"))
-		 (((class color) (min-colors 16)) (:background "red"))
-		 (((class color) (min-colors 8)) (:background "red"))
-		 (((class color grayscale)) (:foreground "grey")) (t (:inverse-video t))))
- 
+         (((class color) (min-colors 88) (background dark)) (:background "#f00"))
+         (((class color) (min-colors 16)) (:background "red"))
+         (((class color) (min-colors 8)) (:background "red"))
+         (((class color grayscale)) (:foreground "grey")) (t (:inverse-video t))))
+
  '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:background "yellow" :foreground "black")) (((class color) (min-colors 8) (background dark)) (:background "blue" :foreground "white")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
  '(next-error ((t (:inherit (region)))))
  '(query-replace ((t (:inherit (isearch))))))
