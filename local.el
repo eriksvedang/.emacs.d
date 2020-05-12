b4a6dcd891daa6974c69e8aa917c589048c5cd6d
Erik Svedang <eriksvedang@Eriks-MacBook-Pro.local>
2020-05-12 22:40:44 +0200

    Move this stuff out.

--- local.el
+++ local.el
@@ -1,13 +1,1 @@
-(setq carp-mode-base-path "/Users/erik/Projects/Carp/emacs/")
-(setq carp-mode-path (concat carp-mode-base-path "carp-mode.el"))
-(setq inf-carp-mode-path (concat carp-mode-base-path "inf-carp-mode.el"))
-(setq carp-flycheck-path (concat carp-mode-base-path "carp-flycheck.el"))
-
-(when (file-exists-p carp-mode-path)
-  (load carp-mode-path))
-
-(when (file-exists-p inf-carp-mode-path)
-  (load inf-carp-mode-path))
-
-(when (file-exists-p carp-flycheck-path)
-  (load carp-flycheck-path))
+(provide 'local)
