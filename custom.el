9bcfddb1ff71a47d9ffd460b448d316708b91d67
Erik Svedäng <erik.svedang@gmail.com>
2020-05-12 14:41:33 +0200

    More packages enabled.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-mode org-bullets magit reveal-in-osx-finder exec-path-from-shell iedit avy find-file-in-project company ido-ubiquitous smex undo-tree haskell-mode smartparens multiple-cursors "use-package" "use-package" use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
