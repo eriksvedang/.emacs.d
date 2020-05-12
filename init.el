1d0216f05508210860684d02c887064aa2c3c1b5
Erik Svedäng <erik.svedang@gmail.com>
2020-05-12 16:41:25 +0200

    Almost done?

--- init.el
+++ init.el
@@ -1,8 +1,4 @@
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (setq custom-file "~/.emacs.d/custom.el")
@@ -10,5 +6,4 @@
 
 ;; Load 'config.org' and evaluate it (will emit 'config.el')
 (require 'org)
-(org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
