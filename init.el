ab9f7e2021198f43be82b1b12ef27f37df2ccb03
Erik Svedäng <erik.svedang@gmail.com>
2020-05-12 22:23:15 +0200

    Support for a local config.

--- init.el
+++ init.el
@@ -7,3 +7,6 @@
 ;; Load 'config.org' and evaluate it (will emit 'config.el')
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
+;; Local config
+(load "local.el")
