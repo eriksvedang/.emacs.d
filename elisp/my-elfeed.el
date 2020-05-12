916a965f3474ce84cc14baf748a0ce125c433c7d
Erik Svedäng <erik.svedang@gmail.com>
2020-05-12 13:12:25 +0200

    Removed pretty much everything, starting over

--- elisp/my-elfeed.el
+++
@@ -1,5 +0,0 @@
-(setq elfeed-feeds
-      '("http://www.skippanudlarna.com/feed/"
-        "http://70sscifiart.tumblr.com/rss"))
-
-(provide 'my-elfeed)
--- init.el
+++ init.el
@@ -1,22 +1,2 @@
-;; Added by Package.el. This must come before configurations of installed packages.
-(package-initialize)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/my-themes")
-(add-to-list 'load-path "~/.emacs.d/elisp")
-
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
-
-(require 'my-packages)
-(require 'my-fix-defaults)
-(require 'my-look-and-feel)
-(require 'my-tabbar)
-(require 'my-keys)
-(require 'my-magit)
-(require 'my-movelines)
-(require 'my-smartparens)
-(require 'my-powerline)
-(require 'my-programming-modes)
-(require 'my-org-mode)
-(require 'my-neotree)
-(require 'my-elfeed)
