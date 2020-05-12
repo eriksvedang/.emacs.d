f908d87fe0c4325390ea3ca15b2d8bae05ec84e6
Erik Svedang <eriksvedang@Eriks-MacBook-Pro.local>
2020-05-12 22:56:46 +0200

    Remove local.el file.

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 (package-initialize)
 
 (setq custom-file "~/.emacs.d/custom.el")
@@ -7,6 +6,3 @@
 ;; Load 'config.org' and evaluate it (will emit 'config.el')
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-
-;; Local config
-(require 'local)
