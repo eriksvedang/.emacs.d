5524b80eb8a162ada5dc61635b97fe76aba249fc
Erik Svedäng <erik@coherence.io>
2020-12-04 16:47:03 +0100

    Ignore bookmarks dir.

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ormolu counsel go-impl company-lsp lsp-ui go-dlv go-imenu company-fuzzy zencoding-mode sgml zencoding dante helm lsp-mode omnisharp omnisharp-emacs flycheck ido-completing-read+ clojure-mode rainbow-mode go-mode org-bullets magit reveal-in-osx-finder exec-path-from-shell iedit avy find-file-in-project company ido-ubiquitous smex undo-tree haskell-mode smartparens multiple-cursors "use-package" "use-package" use-package)))
+   (quote
+    (ghub ormolu counsel go-impl company-lsp lsp-ui go-dlv go-imenu company-fuzzy zencoding-mode sgml zencoding dante helm lsp-mode omnisharp omnisharp-emacs flycheck ido-completing-read+ clojure-mode rainbow-mode go-mode org-bullets magit reveal-in-osx-finder exec-path-from-shell iedit avy find-file-in-project company ido-ubiquitous smex undo-tree haskell-mode smartparens multiple-cursors "use-package" "use-package" use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
