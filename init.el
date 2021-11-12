9a0e7f9b204906d3b1d9fe0122cdd9c42c1840ac
Erik Svedäng <erik@coherence.io>
2021-11-12 14:11:00 +0100

    Move gc-cons-threshold setting to first line of config.

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
+(setq gc-cons-threshold (* 1024 1024 128))
+
 (package-initialize)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
-
-(setq gc-cons-threshold (* 1024 1024 128))
 
 ;; Load 'config.org' and evaluate it (will emit 'config.el')
 (require 'org)
