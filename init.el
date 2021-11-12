945622e76d3df2ba037de0c326e20557d85cc898
Erik Svedäng <erik@coherence.io>
2021-11-12 14:09:53 +0100

    Increase gc-cons-threshold

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+(setq gc-cons-threshold (* 1024 1024 128))
+
 ;; Load 'config.org' and evaluate it (will emit 'config.el')
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
