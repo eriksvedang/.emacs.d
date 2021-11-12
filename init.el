e3f38ec4dfb8181fade71a39955a9af328aeaf92
Erik Svedäng <erik@coherence.io>
2021-11-12 14:14:32 +0100

    More advanced GC settings.

--- init.el
+++ init.el
@@ -1,10 +1,19 @@
+;; Use kind of large GC threshold to increase loading speed
 (setq gc-cons-threshold (* 1024 1024 128))
 
-(package-initialize)
+;; Temporarily disable file-name-handler-alist for faster loading
+(let ((file-name-handler-alist nil))
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+  (package-initialize)
 
-;; Load 'config.org' and evaluate it (will emit 'config.el')
-(require 'org)
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+  (setq custom-file "~/.emacs.d/custom.el")
+  (load custom-file)
+
+  ;; Load 'config.org' and evaluate it (will emit 'config.el')
+  (require 'org)
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
+  )
+
+;; Restore kinda low GC threshold to avoid stutter during normal usage
+(setq gc-cons-threshold (* 1024 1024 8))
